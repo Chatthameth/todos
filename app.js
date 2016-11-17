b4b5125fb23455006cffb22e3321bc0ce2c4236e
Chatthameth <phaky.chatthameth@gmail.com>
2016-11-17 15:36:15 +0700

    edit

---
+++ app.js
@@ -0,0 +1,46 @@
+angular.module('ContactApp', [])
+.service('contactService' , function() {
+  var self = this
+
+  self.contacts = [
+    { title: 'มหาวิทยาลัยบูรพาแจ่มจ้าเจิดขวัญ'},
+    { title: 'คือว่าเรากินส้มตำปูปลาร้าไม่เป็นอะค่ะ'},
+    { title: 'ไม่ได้ตอแหลแค่ดัดจริตเฉยๆ'}
+  ]
+
+  self.list = function () {
+    return self.contacts
+  }
+
+  self.add = function (contact) {
+    self.contacts.push(contact)
+  }
+})
+
+
+.controller('ListContactController',function($scope , contactService ){
+$scope.newContact = {
+  title: ''
+}
+$scope.contacts = contactService.list()
+})
+
+.controller('AddContactController' , function($scope , contactService){
+  $scope.save = function(){
+    var contact = {
+      title: $scope.title }
+
+    contact.title = $scope.title;
+
+    contactService.add(contact);
+    resetForm();
+
+
+  }
+
+    function resetForm(){
+      $scope.title = '';
+
+    }
+
+});
